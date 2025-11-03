6e6754a5071e82ac4bd417ec5eada9bd02483745
Suraj-nishad78 <iamsuraj0737@gmail.com>
2025-11-03 21:03:07 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,17 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import { useEffect } from "react";
-import Image from "./pages/Image";
 
 function App() {
   const [login, setLogin] = useState(false);
-  const [userId, setUserId] = useState(false);
+  const [userId, setUserId] = useState(true);
   const contextData = { login, setLogin, userId, setUserId };
 
   //Work For 03-11-2025
   // 1.Today work on it to make profile section 
   // https://www.sliderrevolution.com/wp-content/uploads/2021/06/boot1.jpg
   // 2.Image uplaod section in the profile & backend patch or put route for update
-  // 3.check for imageurl get byt google is working or not based on this make a if else condition on both frontend & backend 
+  // 3.check for imageurl get but google is working or not based on this make a if else condition on both frontend & backend 
 
 
   useEffect(()=>{
@@ -34,7 +33,6 @@ function App() {
         <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/image" element={<Image />}></Route>
           <Route path="/upgrade" element={<Dashboard />}></Route>
         </Routes>
         <Footer></Footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
+import axios from "axios";
 
 const Navbar = () => {
   const [hideList, setHideList] = useState(false);
   const { login, setLogin, userId, setUserId } = useContext(AppContext);
+  const [hideProfile, setHideProfile] = useState(false);
+  const fileInputRef = useRef(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleClick = () => {
+    fileInputRef.current.click(); // open file picker
+  };
+
+  const handleFileChange = async(e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    try {
+      const data = new FormData();
+      data.append("file", file);
+      data.append("upload_preset", "react_uploads_preset"); // your preset name
+      data.append("cloud_name", "dguxvwjkj");
+
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/dguxvwjkj/image/upload",
+        data
+      );
+      setImageUrl(res.data.secure_url);
+    } catch (e) {
+      console.log("Error occured: ", e);
+    }
+  };
 
   const showList = () => {
     setHideList(!hideList);
@@ -15,10 +43,11 @@ const Navbar = () => {
     setLogin(true);
   };
 
-  const logout = () =>{
+  const logout = () => {
     setUserId("");
+    setHideProfile(false);
     localStorage.clear();
-  }
+  };
 
   return (
     <div className="navbar" style={login ? { opacity: 0.5 } : {}}>
@@ -37,20 +66,53 @@ const Navbar = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/image">Uploads</Link>
-          </li>
-          <li>
             <Link to="/upgrade">Upgrade</Link>
           </li>
-          {userId?
-          <div className="profile-img">
-            {/* <Link onClick={logout}>Logout</Link> */}
-            <img src={assets.profile_icon} onClick={logout} alt="profile" />
-          </div>
-          :
-           <li className="login">
-            <Link onClick={showForm}>Login</Link>
-          </li>}
+          {userId ? (
+            <div className="profile">
+              {/* <Link onClick={logout}>Logout</Link> */}
+              <div className="profile-section">
+                <img
+                  src={imageUrl?imageUrl:assets.profile_icon}
+                  onClick={() => setHideProfile(!hideProfile)}
+                  alt="profile"
+                />
+              </div>
+              {hideProfile && (
+                <div className="profile-details">
+                  <div id="profile-image-update">
+                    <img
+                      src={imageUrl?imageUrl:"https://st4.depositphotos.com/11634452/21365/v/450/depositphotos_213659488-stock-illustration-picture-profile-icon-human-people.jpg"}
+                      loading="lazy"
+                      alt="image"
+                    />
+                    {/* <img
+                      src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000"
+                      loading="lazy"
+                      alt="image"
+                    /> */}
+                    <input
+                      type="file"
+                      accept="image/*"
+                      ref={fileInputRef}
+                      style={{ display: "none" }}
+                      onChange={handleFileChange}
+                    />
+                    <span onClick={handleClick}>+</span>
+                  </div>
+                  <div id="profile-content">
+                    <h1>John Doe</h1>
+                    <p>iamsuraj0737@gmail.com</p>
+                    <button onClick={logout}>Logout</button>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : (
+            <li className="login">
+              <Link onClick={showForm}>Login</Link>
+            </li>
+          )}
         </ul>
         <div className="nav-close" onClick={showList} id="nav-close">
           <i className="ri-close-large-fill"></i>
